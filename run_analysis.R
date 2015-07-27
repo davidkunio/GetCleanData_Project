cb08e5e49b4caf8aec0c5fca3410c8783d12e5e7
davidkunio <davidkunio@gmail.com>
2015-07-26 22:49:08 -0500

    Fixed Errors

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,7 @@
-setwd("~/datascience/3. Obtaining Data/Project/")
-training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
-training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
-training[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+setwd("~/Get_Clean_Data/Project/")
+train = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
 
-testing = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
-testing[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
-testing[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+test = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
 
 activityLabels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
 
@@ -16,7 +12,7 @@ features[,2] = gsub('-std', 'Std', features[,2])
 features[,2] = gsub('[-()]', '', features[,2])
 
 # Merge training and test sets together
-allData = rbind(training, testing)
+allData = rbind(train, test)
 
 # Get only the data on mean and std. dev.
 colsWeWant <- grep(".*Mean.*|.*Std.*", features[,2])
